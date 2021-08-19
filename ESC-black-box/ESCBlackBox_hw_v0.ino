dc603a98c5acc664eec52c4b6551084f60fbf9b6
Grant Geyer <GrantG012@yahoo.com>
2021-08-19 07:44:47 -0500

    use esc class and proper constants

--- ESC-black-box/ESCBlackBox_hw_v0.ino
+++ ESC-black-box/ESCBlackBox_hw_v0.ino
@@ -2,28 +2,38 @@
 // Motor-Testing ESC Black Box
 // [v0.1] [2021.01.22] [Tyler Stagge]
 //    -This just uses the data from the potentiometer to drive a servo (in preparation for driving the lumineer ESC)
+// [v0.2] [2021.01.23] [Tyler Stagge]
+//    -Using ESC class instead of Servo
+//    -Replaced global constants with #define
 
-#include <Servo.h>
+#include <ESC.h>
 
-Servo esc;
+#define PIN_POT (A0)
+#define PIN_LED (7)  // ESC LED override (uses I2C)
+#define PIN_TLM (1)  // ESC telemetry
+#define PIN_PWM (9)
 
-int POT_IN = A0;
+#define ESC_MIN (1000)
+#define ESC_MAX (2000)
+#define ESC_ARM (200)
+
+ESC esc (PIN_PWM, ESC_MIN, ESC_MAX, ESC_ARM);
+
 int potVal = 0;  //10-bit ADC => value ranges from 0 to 1023
-double escSetpoint = 0; //Between 0 and 180
-double CONV_FAC = 0.17578125; //180/1024;
+double escSetpoint = 0; //[us]
 
 void setup(void) {
     Serial.begin(9600);
-    esc.attach(10, 1040, 1960); //Pin 9, Min and Max pulsewidths are 800 and 2000 microseconds
-    //esc.write(90.1);
-    //Serial.println(CONV_FAC);
+    esc.arm();
+    delay(1000);
 }
 
 void loop(void) {
-    potVal = analogRead(POT_IN);
+    potVal = analogRead(PIN_POT);
     //Serial.println(potVal);
 
-    escSetpoint = potVal * CONV_FAC;
+    escSetpoint = map(potVal, 0, 1023, ESC_MIN,ESC_MAX);  // Basically just linear interpolation?
     Serial.println(escSetpoint);
-    esc.write(escSetpoint);
+    esc.speed(escSetpoint);
+    delay(15);
 }
